f5ef12a2e0e23977d71d8606275ec005d1ab7332
Alberto Martinez <josealberto.martinezmarquez@gmail.com>
2019-01-03 17:43:05 -0600

    Se agrega front para publicar

---
+++ backend-colorydestellos/public/build/precache-manifest.d8cae18da431244898136e2970d3c154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b92c92eb8baea68093a",
+    "url": "/static/js/main.2b92c92e.chunk.js"
+  },
+  {
+    "revision": "05557dad0f0d68ded2b5",
+    "url": "/static/js/1.05557dad.chunk.js"
+  },
+  {
+    "revision": "2b92c92eb8baea68093a",
+    "url": "/static/css/main.124cf9f3.chunk.css"
+  },
+  {
+    "revision": "05557dad0f0d68ded2b5",
+    "url": "/static/css/1.7a2048c1.chunk.css"
+  },
+  {
+    "revision": "a3a920acd76fe099198ca3e7f3fe2e4f",
+    "url": "/index.html"
+  }
+];
